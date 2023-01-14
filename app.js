ce7f4cdf18289ca5b6ba7fe32f8273c2f9dadbe8
Mahmood Ahmad <mahmood073@gmail.com>
2023-01-14 02:21:15 +0200

    init home & login

--- app.js
+++ app.js
@@ -1,9 +1,23 @@
 const express = require('express');
-
 const app = express();
+const path = require('path');
+const cookieParser = require('cookie-parser');
 
-app.get('/test', async (req, res) => {
-  res.status(200).json({'test': true});
+app.use(cookieParser(), function(req, res, next) {
+  let _db_sess = req.cookies._db_sess;
+  if (_db_sess && verify(_db_sess) || req.path == '/login') {
+    next();
+  } else {
+    res.redirect('/login');
+  }
+});
+
+app.get('/',function(req,res){
+  res.sendFile(path.join(__dirname + '/pages/home.html'));
+});
+
+app.get('/login',function(req,res){
+  res.sendFile(path.join(__dirname + '/pages/login.html'));
 });
 
 app.listen(80);
